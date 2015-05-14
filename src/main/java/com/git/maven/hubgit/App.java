d436a9a768d2e5ed89d075e3bb6b3de9a382c122
Administrator <Administrator@LT0024848.in.ril.com>
2015-05-14 15:23:49 +0530

    first commit from eclipse

---
+++ src/main/java/com/git/maven/hubgit/App.java
@@ -0,0 +1,13 @@
+package com.git.maven.hubgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
